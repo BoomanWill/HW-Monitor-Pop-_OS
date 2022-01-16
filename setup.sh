c7af212dc6e9887333f979a417282c1d59950ffd
BoomanWill <williamhstanger@btinternet.com>
2022-01-16 21:09:38 +0000

    added setup.s

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install python3-pip
+sudo apt-get install wmctrl
+sudo pip install keyboard
+sudo pip install psutil
+pip install psutil
+sudo pip install matplotlib
+pip install matplotlib
+
+
+sudo mkdir ~/.hwmonitor
+sudo mv hwmonitor.py ~/.hwmonitor/hwmonitor.py
+sudo mv hwmonitor ~/.hwmonitor/hwmonitor
+sudo ln -s ~/.hwmonitor/hwmonitor /usr/bin/hwmonitor
